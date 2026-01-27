437b026128213dae467cba323b43613504e681f3
carbelrod97-svg <carbelrod97@gmail.com>
2026-01-27 15:14:19 +0100

    Delete story_content directory

--- story_content/6m2OeiP6bdy_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalLoadJsAsset('story_content/6m2OeiP6bdy_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.109.36125.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.082%0D%0AWelcome%20to%20Carlos%20Bele%C3%B1o%20e-Learning%20Specialist%20CV%0D%0A%0D%0A"}]})
